89a3916f746515ffde7d494c6464eb7867f2183e
Nestor Gonzalez <nestor.ge222@gmail.com>
2020-04-09 20:14:54 -0700

    uso de una cookies

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
-from flask import Flask, request
+from flask import Flask, request, make_response
 
 app = Flask(__name__)
 
+
+@app.route('/')
+def index():
+    user_ip = request.remote_addr
+    response = make_response(redirect('/hello'))
+    response.set_cookie('user_ip','user_ip')
+    return response
+
+
 @app.route('/')
 def hello():
-    user_ip = request.remote_addr
+    user_ip = request.cookies.get('user_ip')
     return 'Hello work flask, tu ip es {}'.format(user_ip)
