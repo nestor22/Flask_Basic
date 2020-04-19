8bf3859234ef684de6f2a81bebffe77584a55367
Nestor Gonzalez <nestor.ge222@gmail.com>
2020-04-18 21:21:36 -0700

    integracion de fabrica y bluprints para login y otros

--- tests/test_base.py
+++ tests/test_base.py
@@ -20,11 +20,31 @@ class MainTest(TestCase):
     def test_hello_get(self):  
         response = self.client.get(url_for('hello'))
         self.assert200(response)
-    
+
     def test_hello_post(self):
-        fake_user = {
-            'username':'fake',
-            'password':'12345678'
-        }
+        # fake_user = {
+        #     'username':'fake',
+        #     'password':'12345678'
+        # }
         response = self.client.post(url_for('hello', data = fake_user)
+        self.assertTrue(response.status_code, 405)
+        
+
+    def test_auth_blueprint_exists(self):
+        self.assertIn('auth', self.app.blueprints) 
+
+    def test_auth_login_get(self):
+        response = self.client.get(url_for('auth.login'))
+        self.assert200(response)
+    
+    def test_auth_login_template(self):
+        self.client.get(url_for('auth.login'))
+        self.assertTemplateUserd('login.html')
+    
+    def test_auth_login_post(self):
+        fake_form = {
+            'username':'fake',
+            'password':'fake-password'
+        }
+        response = self.client.post(url_for('auth.login', fake_form))
         self.assertRedirects(response, url_for('index'))
